c7e0b68b31b3ad56453e7d6d3563a96be78df6c2
FazioD <faziod@uw.edu>
2015-11-17 17:10:31 -0800

    adding changes per feedback

--- Davide_guessing.js
+++ Davide_guessing.js
@@ -1,26 +1,25 @@
-var number = 37;
-    var guess = prompt("How old am I?");
-    if (number === guess) {
-      alert("You guessed the right number!");
+    var guessAge = prompt("How old am I?");
+    if (guessAge === "37" || guessAge === "thirty seven" || guessAge === "thirty-seven") {
+      alert("Your guess is correct!");
     } else {
     alert("Your guess was incorrect");
   }
-    var guessState=prompt("Where does Davide live?");
+    var guessState = prompt("Where does Davide live?");
     if (guessState === "Seattle" || guessState === "seattle" || guessState === "SEATTLE") {
-      alert("Your guess is correct!");
+      alert("Your guess is correct! Davide lives in Seattle");
    } else {
     alert("Your guess is incorrect");
   }   
-      var veteran=prompt("Is Davide a veteran?");
+      var veteran = prompt("Is Davide a veteran?");
     if (veteran === "yes" || veteran === "Y" || veteran === "YES" || veteran === "y"){
-        alert("Your guess is correct!");
+        alert("Your guess is correct! Davide is a veteran");
     }
     else {
     alert("Your guess is incorrect");
   }
-      var guessSiblings=prompt("Does David have any siblings?");
-     if (yes === "yes") || yes ==="Y" || yes === "YES"
-        alert("Your guess is correct!");
+      var siblings = prompt("Does David have any siblings?");
+     if (siblings === "yes" || siblings === "Y" || siblings === "YES"){
+        alert("Your guess is correct! Davide has siblings");
     }
     else {
       alert("Your guess is incorrect");
